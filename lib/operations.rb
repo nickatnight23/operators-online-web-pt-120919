72010d7f991e99a6532b7c5c4b2fcfdb526003d9
Latricia Nickelberry <tricianickelberry@gmail.com>
2019-12-11 19:30:08 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
 def unsafe?(speed)
+  if speed > 60
+    "speeding"
+  else
+    "not speeding"
 
 end
 
 
 
 def not_safe?(speed)
-	
+	not_safe= 30
+	not_safe < 30 ? "slow" : "speeding"
 end
 	
 
